f43b72522d32f1df7563a44bc37b3d6836920da4
KarakulovSS <karakulov.web.dev@gmail.com>
2023-07-28 23:37:20 +0300

    https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/1006442197/

---
+++ app.js
@@ -0,0 +1,1 @@
+// require('./index${leetcode question number}.js')
---
+++ index3.js
@@ -0,0 +1,21 @@
+const lengthOfLongestSubstring = (s) => {
+    let windowStart = 0
+    let windowEnd = 0
+    let set = new Set()
+    let maxLength = 0
+
+    if (s.length === 0) return 0;
+    if (s.length === 1) return 1;
+
+    while (windowEnd < s.length) {
+        if (set.has(s[windowEnd])) {
+            set.delete(s[windowStart])
+            windowStart++
+        } else {
+            set.add(s[windowEnd])
+            if (set.size > maxLength) maxLength = set.size
+            windowEnd++
+        }
+    }
+    return maxLength
+}
